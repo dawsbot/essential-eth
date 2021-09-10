f3a93963a9f2b5f781d750c71afb4b3c4f36c3e1
Dawson Botsford <dawsonbotsford@gmail.com>
2021-09-10 14:19:16 -0700

    Prettier everything

--- jest.config.js
+++ jest.config.js
@@ -1,12 +1,10 @@
 module.exports = {
-  "roots": [
-    "<rootDir>/test"
-  ],
-  "testMatch": [
+  roots: ["<rootDir>/test"],
+  testMatch: [
     "**/__tests__/**/*.+(ts|tsx|js)",
-    "**/?(*.)+(spec|test).+(ts|tsx|js)"
+    "**/?(*.)+(spec|test).+(ts|tsx|js)",
   ],
-  "transform": {
-    "^.+\\.(ts|tsx)$": "ts-jest"
+  transform: {
+    "^.+\\.(ts|tsx)$": "ts-jest",
   },
-}
+};
