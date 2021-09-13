66b681214a73be660937787e5a49baa129fa0364
Dawson Botsford <dawsonbotsford@gmail.com>
2021-09-12 18:23:22 -0700

    Update docs

--- src/shared/tiny-big/tiny-big.ts
+++ src/shared/tiny-big/tiny-big.ts
@@ -1,10 +1,10 @@
 import Big from 'big.js';
 import { scientificStrToDecimalStr } from './helpers';
 
-// https://stackoverflow.com/questions/1685680/how-to-avoid-scientific-notation-for-large-numbers-in-javascript/1685917
-export const stripScientificNotation = (num: number) =>
-  num.toLocaleString('fullwide', { useGrouping: false });
-
+/**
+ * A wrapper around big.js which expands scientific notation. This is the return type of every operation on ether, wei, etc.
+ * The only important things to know are the "toString" and "toNumber" functions
+ */
 export class TinyBig {
   // TODO: replace usage of big.js with a custom and more minimal function
   __value: Big;
