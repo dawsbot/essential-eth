eb4cdfc70466be4511ffb668c9e4e12570f4247c
Dawson Botsford <dawsonbotsford@gmail.com>
2021-09-16 23:56:16 -0700

    add size calculating script

---
+++ scripts/calculate-bundle-size.js
@@ -0,0 +1,5 @@
+const path = require('path');
+const { getPackageStats } = require('package-build-stats');
+getPackageStats(path.join(__dirname, '..')).then((results) => {
+  console.log(results);
+});
