b16f5cd3f2cdfb4fd0c7a762f50dbd38ee5cf19c
Dawson Botsford <dawsonbotsford@gmail.com>
2021-09-17 00:11:02 -0700

    remove bundle size calculation

--- scripts/calculate-bundle-size.js
+++ scripts/calculate-bundle-size.js
@@ -1,5 +1,5 @@
 const path = require('path');
 const { getPackageStats } = require('package-build-stats');
-getPackageStats(path.join(__dirname, '..')).then((results) => {
+getPackageStats(path.join(__dirname, '..'), {}).then((results) => {
   console.log(results);
 });
