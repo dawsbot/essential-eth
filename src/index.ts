30146c7f89cd36ba18b5b8760890d38b0efd6614
Dawson Botsford <dawsonbotsford@gmail.com>
2021-10-01 22:58:26 -0700

    export all types used internally

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,9 @@
+import { Transaction } from 'ethers';
 import { Contract } from './classes/Contract';
 import { JsonRpcProvider, jsonRpcProvider } from './providers/JsonRpcProvider';
 import { tinyBig, TinyBig } from './shared/tiny-big/tiny-big';
-import { JSONABI } from './types/Contract.types';
+import { Block } from './types/block.types';
+import { JSONABI, JSONABIArgument } from './types/Contract.types';
 import { etherToWei } from './utils/ether-to-wei';
 import { isAddress } from './utils/is-address';
 import { toChecksumAddress } from './utils/to-checksum-address';
@@ -18,4 +20,7 @@ export {
   TinyBig,
   Contract,
   JSONABI,
+  Block,
+  JSONABIArgument,
+  Transaction,
 };
