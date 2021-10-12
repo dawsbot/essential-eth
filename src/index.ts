22398fbf14474da8ca5bf39212861498f4a2afcc
Dawson Botsford <3408480+dawsbot@users.noreply.github.com>
2021-10-11 23:50:18 -0700

    Remove accidental ethers type export

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,13 @@
-import { Transaction } from 'ethers';
 import { Contract } from './classes/Contract';
 import { JsonRpcProvider, jsonRpcProvider } from './providers/JsonRpcProvider';
 import { tinyBig, TinyBig } from './shared/tiny-big/tiny-big';
 import { Block } from './types/block.types';
-import { JSONABI, JSONABIArgument } from './types/Contract.types';
+import {
+  ContractTypes,
+  JSONABI,
+  JSONABIArgument,
+} from './types/Contract.types';
+import { Transaction } from './types/transaction.types';
 import { etherToWei } from './utils/ether-to-wei';
 import { isAddress } from './utils/is-address';
 import { toChecksumAddress } from './utils/to-checksum-address';
@@ -17,10 +21,13 @@ export {
   etherToWei,
   weiToEther,
   tinyBig,
+  /* classes */
   TinyBig,
   Contract,
+  /* types */
   JSONABI,
   Block,
   JSONABIArgument,
   Transaction,
+  ContractTypes,
 };
