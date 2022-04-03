6fd2459fee2cfc481cdc6b5d2210dd55d049a57e
Dawson Botsford <dawsonbotsford@gmail.com>
2022-04-02 17:59:01 -0700

    Revert multi-json-rpc #44 (#56)
    * revert #44
    
    * extend jest timeout

--- test/global-setup.js
+++ test/global-setup.js
@@ -3,6 +3,6 @@ const { setup } = require('jest-dev-server');
 module.exports = async function globalSetup() {
   await setup({
     command: `node test/server`,
-    launchTimeout: 10000,
+    launchTimeout: 15000,
   });
 };
