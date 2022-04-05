935f94b3b6e7b2c81559626e1bd44b72223cde97
Ari Gibson <hi@arimgibson.com>
2022-04-05 09:06:29 -0600

    :pencil2: Fixed inconsistencies and typos

--- src/utils/ether-to-wei.ts
+++ src/utils/ether-to-wei.ts
@@ -3,6 +3,8 @@ import { TinyBig, tinyBig } from '../shared/tiny-big/tiny-big';
 import { validateType } from '../shared/validate-type';
 
 /**
+ * Convert Ether to Wei
+ *
  * Similar to ["parseEther" in ethers.js](https://docs.ethers.io/v4/api-utils.html#ether-strings-and-wei)
  *
  * Similar to ["toWei" in web3](https://web3js.readthedocs.io/en/v1.2.11/web3-utils.html?highlight=towei#towei)
