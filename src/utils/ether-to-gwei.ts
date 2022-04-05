30dae0cf3c000b6dbdd04ecec2296dcc1fd19472
Ari Gibson <hi@arimgibson.com>
2022-04-05 10:30:48 -0600

    :sparkles: Added etherToGwei and gweiToEther functions

---
+++ src/utils/ether-to-gwei.ts
@@ -0,0 +1,34 @@
+import Big from 'big.js';
+import { TinyBig, tinyBig } from '../shared/tiny-big/tiny-big';
+import { validateType } from '../shared/validate-type';
+
+/**
+ * Convert from Ether to Gwei
+ *
+ * No direct equivalent in ether.js; requires multiple functions to achieve.
+ *
+ * No direct equivalent in web3; requires multiple functions to achieve.
+ *
+ * @example
+ * ```javascript
+ * etherToGwei('1000').toString()
+ * // '1000000000000'
+ * etherToGwei(1000).toString()
+ * // '1000000000000'
+ * ```
+ *
+ * @example
+ * ```javascript
+ * etherToGwei('1000').toNumber()
+ * // 1000000000000
+ * etherToGwei(1000).toNumber()
+ * // 1000000000000
+ * ```
+ */
+export function etherToGwei(
+  etherQuantity: string | number | TinyBig | Big,
+): TinyBig {
+  validateType(etherQuantity, ['string', 'number', 'object']);
+  const result = tinyBig(etherQuantity).times('1000000000');
+  return tinyBig(result);
+}
---
+++ src/utils/tests/gwei-to-ether.test.ts
@@ -0,0 +1,27 @@
+import Big from 'big.js';
+import { gweiToEther, tinyBig } from '../../index';
+
+describe('gwei-to-ether', () => {
+  it('happy path', () => {
+    expect(gweiToEther('100000000000.0').toString()).toBe('100');
+    expect(gweiToEther(100000000000.0).toString()).toBe('100');
+    expect(gweiToEther('1000000000000.0').toNumber()).toBe(1000);
+    expect(gweiToEther(1000000000000.0).toNumber()).toBe(1000);
+
+    expect(gweiToEther(tinyBig('1000000000000.0')).toNumber()).toBe(1000);
+    expect(gweiToEther(tinyBig(1000000000000.0)).toNumber()).toBe(1000);
+    expect(gweiToEther(Big('1000000000000.0')).toNumber()).toBe(1000);
+    expect(gweiToEther(Big(1000000000000.0)).toNumber()).toBe(1000);
+  });
+
+  it('wrong types', () => {
+    expect(() => {
+      // @ts-expect-error should not accept boolean
+      gweiToEther(false);
+    }).toThrow();
+    expect(() => {
+      // @ts-expect-error should not accept array
+      gweiToEther([1, 2, 3]);
+    }).toThrow();
+  });
+});
