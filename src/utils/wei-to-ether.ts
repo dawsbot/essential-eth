0a2c1d1e6dba3cd2094cd3379336b2d939b86eb1
Ari Gibson <hi@arimgibson.com>
2022-04-05 17:51:58 -0600

    :pencil2: Update references to web3.js and ethers.js docs (#62)
    * :pencil2: Update refs to ethers and web3 docs
    
    * :pencil2: Change web3 to web3.js for clarification
    
    Co-authored-by: daws.eth <dawsonbotsford@gmail.com>

--- src/utils/wei-to-ether.ts
+++ src/utils/wei-to-ether.ts
@@ -6,9 +6,9 @@ import { TinyBig } from './../shared/tiny-big/tiny-big';
 /**
  * Convert from Ether to Wei
  *
- * Similar to ["formatEther" in ethers.js](https://docs.ethers.io/v4/api-utils.html#ether-strings-and-wei)
+ * Similar to ["formatEther" in ethers.js](https://docs.ethers.io/v5/api/utils/display-logic/#utils-formatEther)
  *
- * Similar to ["fromWei" in web3](https://web3js.readthedocs.io/en/v1.2.11/web3-utils.html#fromwei)
+ * Similar to ["fromWei" in web3.js](https://web3js.readthedocs.io/en/v1.7.1/web3-utils.html#fromwei)
  *
  * @example
  * ```javascript
