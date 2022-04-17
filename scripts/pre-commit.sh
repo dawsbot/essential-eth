052b562629178435f5d7598ec82739af4e31df6c
Dawson Botsford <dawsonbotsford@gmail.com>
2022-04-16 23:40:59 -0700

    🏎 Move to npm lockfile v2 (#75)
    Supports faster installs on npm 7+

--- scripts/pre-commit.sh
+++ scripts/pre-commit.sh
@@ -1,6 +1,6 @@
 npx npm-check-updates -u  "/.*lint.*$/"
 npx npm-check-updates -u  "/.*jest.*$/"
 npx npm-check-updates -u  "/.*big.*$/"
-npx npm-check-updates -u ethers typescript web3 prettier
-npm install
+npm i
+npm i ethers@latest typescript@latest web3@latest prettier@latest
 
