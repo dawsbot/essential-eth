c24119cc5dece80a0d2969015d449659d5389606
Ari Gibson <hi@arimgibson.com>
2022-04-19 14:36:09 -0600

    Untested solidityKeccak256 implementation

--- src/utils/solidity-keccak256.ts
+++ src/utils/solidity-keccak256.ts
@@ -1,3 +1,81 @@
+import { tinyBig } from "../shared/tiny-big/tiny-big";
+import { Buffer } from "buffer";
+import { Keccak } from "sha3";
+
+// Importing from ethers for the time being until an optimized essential-eth option can be made
+import { arrayify, zeroPad, concat, hexlify } from "./bytes";
+import { logger } from "../logger/logger";
+
+const regexBytes = new RegExp("^bytes([0-9]+)$");
+const regexNumber = new RegExp("^(u?int)([0-9]*)$");
+const regexArray = new RegExp("^(.*)\\[([0-9]*)\\]$");
+
+const Zeros = "0000000000000000000000000000000000000000000000000000000000000000";
+
+function pack(type: string, value: any, isArray?: boolean): Uint8Array {
+  switch(type) {
+      case "address":
+          if (isArray) { return zeroPad(value, 32); }
+          return arrayify(value);
+      case "string":
+          return Buffer.from(value);
+      case "bytes":
+          return arrayify(value);
+      case "bool":
+          value = (value ? "0x01": "0x00");
+          if (isArray) { return zeroPad(value, 32); }
+          return arrayify(value);
+  }
+
+  let match =  type.match(regexNumber);
+  if (match) {
+      //let signed = (match[1] === "int")
+      let size = parseInt(match[2] || "256")
+
+      if ((match[2] && String(size) !== match[2]) || (size % 8 !== 0) || size === 0 || size > 256) {
+          logger.throwArgumentError("invalid number type", "type", type)
+      }
+
+      if (isArray) { size = 256; }
+
+      // value = BigNumber.from(value).toTwos(size);
+      value = tinyBig(value)
+
+      return zeroPad(value, size / 8);
+  }
+
+  match = type.match(regexBytes);
+  if (match) {
+      const size = parseInt(match[1]);
+
+      if (String(size) !== match[1] || size === 0 || size > 32) {
+          logger.throwArgumentError("invalid bytes type", "type", type)
+      }
+      if (arrayify(value).byteLength !== size) {
+          logger.throwArgumentError(`invalid value for ${ type }`, "value", value)
+      }
+      if (isArray) { return arrayify((value + Zeros).substring(0, 66)); }
+      return value;
+  }
+
+  match = type.match(regexArray);
+  if (match && Array.isArray(value)) {
+      const baseType = match[1];
+      const count = parseInt(match[2] || String(value.length));
+      if (count != value.length) {
+          logger.throwArgumentError(`invalid array length for ${ type }`, "value", value)
+      }
+      const result: Array<Uint8Array> = [];
+      value.forEach(function(value) {
+          result.push(pack(baseType, value, true));
+      });
+      return concat(result);
+  }
+
+  return logger.throwArgumentError("invalid type", "type", type)
+}
+
+
 export function solidityKeccak256(
   types: ReadonlyArray<string>,
   values: ReadonlyArray<any>,
@@ -10,11 +88,12 @@ export function solidityKeccak256(
 
   const tight: Array<Uint8Array> = [];
   types.forEach((type, index) => {
-    tight.push();
+    tight.push(pack(type, values[index]));
   });
 
-  // make sure types and values are same length
-  // create new Uint8Array
-  // concat array and turn into hex
-  return '';
+  const hexData: string = hexlify(concat(tight));
+  
+  const hash = new Keccak(256)
+  hash.update(Buffer.from(hexData, 'hex'));
+  return hash.digest().toString()
 }
