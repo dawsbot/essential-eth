6df78364cf1b2a181f81a7d4b2f4e079cf5c4a96
Ari Gibson <hi@arimgibson.com>
2022-04-19 16:28:40 -0600

    Format, compare expected types to actual types

--- src/utils/solidity-keccak256.ts
+++ src/utils/solidity-keccak256.ts
@@ -1,80 +1,96 @@
-import { tinyBig } from "../shared/tiny-big/tiny-big";
-import { Buffer } from "buffer";
-import { Keccak } from "sha3";
+import { Buffer } from 'buffer';
+import { Keccak } from 'sha3';
+import { logger } from '../logger/logger';
+import { tinyBig } from '../shared/tiny-big/tiny-big';
+import { arrayify, concat, hexlify, zeroPad } from './bytes';
 
-// Importing from ethers for the time being until an optimized essential-eth option can be made
-import { arrayify, zeroPad, concat, hexlify } from "./bytes";
-import { logger } from "../logger/logger";
+const regexBytes = new RegExp('^bytes([0-9]+)$');
+const regexNumber = new RegExp('^(u?int)([0-9]*)$');
+const regexArray = new RegExp('^(.*)\\[([0-9]*)\\]$');
 
-const regexBytes = new RegExp("^bytes([0-9]+)$");
-const regexNumber = new RegExp("^(u?int)([0-9]*)$");
-const regexArray = new RegExp("^(.*)\\[([0-9]*)\\]$");
+const Zeros =
+  '0000000000000000000000000000000000000000000000000000000000000000';
 
-const Zeros = "0000000000000000000000000000000000000000000000000000000000000000";
-
+// pack function copied from ethers with adjustments
 function pack(type: string, value: any, isArray?: boolean): Uint8Array {
-  switch(type) {
-      case "address":
-          if (isArray) { return zeroPad(value, 32); }
-          return arrayify(value);
-      case "string":
-          return Buffer.from(value);
-      case "bytes":
-          return arrayify(value);
-      case "bool":
-          value = (value ? "0x01": "0x00");
-          if (isArray) { return zeroPad(value, 32); }
-          return arrayify(value);
+  switch (type) {
+    case 'address':
+      if (isArray) {
+        return zeroPad(value, 32);
+      }
+      return arrayify(value);
+    case 'string':
+      return Buffer.from(value);
+    case 'bytes':
+      return arrayify(value);
+    case 'bool':
+      value = value ? '0x01' : '0x00';
+      if (isArray) {
+        return zeroPad(value, 32);
+      }
+      return arrayify(value);
   }
 
-  let match =  type.match(regexNumber);
+  let match = type.match(regexNumber);
   if (match) {
-      //let signed = (match[1] === "int")
-      let size = parseInt(match[2] || "256")
+    //let signed = (match[1] === "int")
+    let size = parseInt(match[2] || '256');
 
-      if ((match[2] && String(size) !== match[2]) || (size % 8 !== 0) || size === 0 || size > 256) {
-          logger.throwArgumentError("invalid number type", "type", type)
-      }
+    if (
+      (match[2] && String(size) !== match[2]) ||
+      size % 8 !== 0 ||
+      size === 0 ||
+      size > 256
+    ) {
+      logger.throwArgumentError('invalid number type', 'type', type);
+    }
 
-      if (isArray) { size = 256; }
+    if (isArray) {
+      size = 256;
+    }
 
-      // value = BigNumber.from(value).toTwos(size);
-      value = tinyBig(value)
+    // value = BigNumber.from(value).toTwos(size);
+    value = tinyBig(value);
 
-      return zeroPad(value, size / 8);
+    return zeroPad(value, size / 8);
   }
 
   match = type.match(regexBytes);
   if (match) {
-      const size = parseInt(match[1]);
+    const size = parseInt(match[1]);
 
-      if (String(size) !== match[1] || size === 0 || size > 32) {
-          logger.throwArgumentError("invalid bytes type", "type", type)
-      }
-      if (arrayify(value).byteLength !== size) {
-          logger.throwArgumentError(`invalid value for ${ type }`, "value", value)
-      }
-      if (isArray) { return arrayify((value + Zeros).substring(0, 66)); }
-      return value;
+    if (String(size) !== match[1] || size === 0 || size > 32) {
+      logger.throwArgumentError('invalid bytes type', 'type', type);
+    }
+    if (arrayify(value).byteLength !== size) {
+      logger.throwArgumentError(`invalid value for ${type}`, 'value', value);
+    }
+    if (isArray) {
+      return arrayify((value + Zeros).substring(0, 66));
+    }
+    return value;
   }
 
   match = type.match(regexArray);
   if (match && Array.isArray(value)) {
-      const baseType = match[1];
-      const count = parseInt(match[2] || String(value.length));
-      if (count != value.length) {
-          logger.throwArgumentError(`invalid array length for ${ type }`, "value", value)
-      }
-      const result: Array<Uint8Array> = [];
-      value.forEach(function(value) {
-          result.push(pack(baseType, value, true));
-      });
-      return concat(result);
+    const baseType = match[1];
+    const count = parseInt(match[2] || String(value.length));
+    if (count != value.length) {
+      logger.throwArgumentError(
+        `invalid array length for ${type}`,
+        'value',
+        value,
+      );
+    }
+    const result: Array<Uint8Array> = [];
+    value.forEach(function (value) {
+      result.push(pack(baseType, value, true));
+    });
+    return concat(result);
   }
 
-  return logger.throwArgumentError("invalid type", "type", type)
+  return logger.throwArgumentError('invalid type', 'type', type);
 }
-
 
 export function solidityKeccak256(
   types: ReadonlyArray<string>,
@@ -86,14 +102,22 @@ export function solidityKeccak256(
     );
   }
 
+  types.forEach((type, index) => {
+    if (typeof values[index] != type)
+      throw new Error(`
+    Specified type doesn't match actual type of value. (type=${type}, value=${
+        values[index]
+      }, typeof value=${typeof values[index]})`);
+  });
+
   const tight: Array<Uint8Array> = [];
   types.forEach((type, index) => {
     tight.push(pack(type, values[index]));
   });
 
   const hexData: string = hexlify(concat(tight));
-  
-  const hash = new Keccak(256)
+
+  const hash = new Keccak(256);
   hash.update(Buffer.from(hexData, 'hex'));
-  return hash.digest().toString()
+  return hash.digest().toString();
 }
