853ba267654bb7d1b29560dbadfb3538a4ba4e6b
daws.eth <dawsonbotsford@gmail.com>
2022-04-21 17:06:18 -0700

    implement basic fallthrough behavior

--- src/providers/FallthroughProvider.ts
+++ src/providers/FallthroughProvider.ts
@@ -1,16 +1,34 @@
-// import { logger } from '../logger/logger';
 import { logger } from '../logger/logger';
 import { BaseProvider } from './BaseProvider';
 /**
  * Similar to ethers FallbackProvider, but with simpler fallthrough behavior
  */
 export class FallthroughProvider extends BaseProvider {
+  // index of current trusted rpc url
+  private rpcUrlCounter = 0;
   selectRpcUrl(): string {
-    return this._rpcUrls[0];
+    return this._rpcUrls[this.rpcUrlCounter];
   }
-  post(body: Record<string, unknown>): Promise<any> {
-    return this._post(body);
-  }
+
+  post = (body: Record<string, unknown>): Promise<any> => {
+    // while failing post, add to rpcUrlCounter and post again
+    const genesisCount = this.rpcUrlCounter;
+
+    const recursivePostRetry = (): Promise<any> => {
+      // TODO: cancel slow-responses
+      const res = this._post(body).catch((e) => {
+        // add one and handle array overflow
+        this.rpcUrlCounter = (this.rpcUrlCounter + 1) % this._rpcUrls.length;
+        // we've already tried this rpc, throw for good
+        if (this.rpcUrlCounter === genesisCount) {
+          throw e;
+        }
+        return recursivePostRetry();
+      });
+      return res;
+    };
+    return recursivePostRetry();
+  };
 
   constructor(rpcUrls: string[]) {
     if (!Array.isArray(rpcUrls)) {
--- src/providers/test/fallthrough-provider/get-gas-price.test.ts
+++ src/providers/test/fallthrough-provider/get-gas-price.test.ts
@@ -6,11 +6,15 @@ const rpcUrl = rpcUrls.mainnet;
 describe('provider.getGasPrice', () => {
   it('should match ethers and essential-eth', async () => {
     const essentialEthProvider = new FallthroughProvider([
-      'https://bad.com',
+      'https://bad-123123123123.com',
+      'https://bad-223123123123.com',
+      'https://bad-323123123123.com',
+      'https://bad-423123123123.com',
+      'https://bad-523123123123.com',
       rpcUrl,
     ]);
     const block = await essentialEthProvider.getBlock(14631185);
 
-    expect(block.gasUsed).toBe(100);
+    expect(block.gasUsed).toBe(2429588);
   });
 });
