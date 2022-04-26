d585044c42411aadc7dc3989c486fb68e53e203d
daws.eth <dawsonbotsford@gmail.com>
2022-04-26 09:25:53 -0700

    Customize timeout duration

--- src/providers/FallthroughProvider.ts
+++ src/providers/FallthroughProvider.ts
@@ -10,27 +10,40 @@ const promiseTimeout = (prom: Promise<any>, time: number) =>
     ),
   ]);
 
+interface ConstructorOptions {
+  timeoutDuration?: number;
+}
+const DEFAULT_TIMEOUT_DURATION = 8000;
+
 /**
  * Similar to ethers FallbackProvider, but with simpler fallthrough behavior
  */
 export class FallthroughProvider extends BaseProvider {
   // index of current trusted rpc url
   private rpcUrlCounter = 0;
-  /**
-   * Time in ms for which any request should be considered stale
-   */
-  private readonly TIMEOUT_DURATION = 8000;
-  selectRpcUrl(): string {
+  private timeoutDuration: number;
+  public selectRpcUrl(): string {
     return this._rpcUrls[this.rpcUrlCounter];
   }
 
-  post = (body: Record<string, unknown>): Promise<any> => {
+  constructor(rpcUrls: string[], options: ConstructorOptions = {}) {
+    if (!Array.isArray(rpcUrls)) {
+      logger.throwError('Array required', { rpcUrls });
+    }
+    if (rpcUrls.length <= 1) {
+      logger.throwError('More than one rpcUrl is required', { rpcUrls });
+    }
+    super(rpcUrls);
+    this.timeoutDuration = options.timeoutDuration || DEFAULT_TIMEOUT_DURATION;
+  }
+
+  public post = (body: Record<string, unknown>): Promise<any> => {
     // while failing post, add to rpcUrlCounter and post again
     const genesisCount = this.rpcUrlCounter;
 
     const recursivePostRetry = (): Promise<any> => {
       // Times out request
-      const res = promiseTimeout(this._post(body), this.TIMEOUT_DURATION).catch(
+      const res = promiseTimeout(this._post(body), this.timeoutDuration).catch(
         (e) => {
           // add one and handle array overflow
           this.rpcUrlCounter = (this.rpcUrlCounter + 1) % this._rpcUrls.length;
@@ -45,14 +58,4 @@ export class FallthroughProvider extends BaseProvider {
     };
     return recursivePostRetry();
   };
-
-  constructor(rpcUrls: string[]) {
-    if (!Array.isArray(rpcUrls)) {
-      logger.throwError('Array required', { rpcUrls });
-    }
-    if (rpcUrls.length <= 1) {
-      logger.throwError('More than one rpcUrl is required', { rpcUrls });
-    }
-    super(rpcUrls);
-  }
 }
--- src/providers/test/fallthrough-provider/get-gas-price.test.ts
+++ src/providers/test/fallthrough-provider/get-gas-price.test.ts
@@ -3,17 +3,46 @@ import { rpcUrls } from '../rpc-urls';
 
 const rpcUrl = rpcUrls.mainnet;
 
-describe('provider.getGasPrice', () => {
-  it('should match ethers and essential-eth', async () => {
-    const essentialEthProvider = new FallthroughProvider([
-      'https://bad-123123123123.com',
-      // url with request delayed by 20 seconds
-      'https://flash-sgkl.onrender.com/delay/20000',
-      'https://bad-523123123123.com',
-      rpcUrl,
-    ]);
-    const block = await essentialEthProvider.getBlock(14631185);
+function timePromise(fn: () => Promise<any>): Promise<number> {
+  const onPromiseDone = () => performance.now() - start;
 
+  const start = performance.now();
+  return fn().then(onPromiseDone, onPromiseDone);
+}
+
+describe('provider.getGasPrice', () => {
+  it('should fallthrough on several types of invalid urls', async () => {
+    const essentialEthProvider = new FallthroughProvider(
+      [
+        'https://bad-123123123123.com',
+        // url with request delayed by 20 seconds
+        'https://flash-sgkl.onrender.com/delay/20000',
+        'https://bad-523123123123.com',
+        rpcUrl,
+      ],
+      { timeoutDuration: 1000 },
+    );
+    const block = await essentialEthProvider.getBlock(14631185);
     expect(block.gasUsed).toBe(2429588);
+  });
+  it('should fallthrough after timeout linearly', async () => {
+    const essentialEthProvider = new FallthroughProvider(
+      [
+        'https://flash-sgkl.onrender.com/delay/10000',
+        // url with request delayed by 20 seconds
+        'https://flash-sgkl.onrender.com/delay/10000',
+        'https://bad-523123123123.com',
+        rpcUrl,
+      ],
+      { timeoutDuration: 1000 },
+    );
+    await timePromise(() => essentialEthProvider.getBlock(14631000)).then(
+      (duration) => {
+        // times out the first two requests in 2000 ms each
+        expect(duration).toBeGreaterThan(2000);
+        // finished the last valid request within two seconds
+        expect(duration).toBeLessThan(4000);
+      },
+    );
   });
 });
