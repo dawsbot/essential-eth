da1e9627e647320f0d0d97dafe38e4663668a733
daws.eth <dawsonbotsford@gmail.com>
2022-04-26 09:53:07 -0700

    speed up husky update scripts

--- scripts/pre-commit.sh
+++ scripts/pre-commit.sh
@@ -1,4 +1,4 @@
-npx npm-check-updates -u  "/.*lint.*$/"
+npx npm-check-updates -u  "/.*eslint.*$/"
 npx npm-check-updates -u  "/.*big.*$/"
 npm i
 npm i ethers@latest typescript@latest web3@latest prettier@latest
