26ed8b1c04f52a066a6b85d84a413ba58c942e2c
Ari Gibson <hi@arimgibson.com>
2022-04-29 14:48:04 -0600

    ✨Added `getTransactionReceipt` function (#106)
    * 📝 Initial documentation for getTransactionReceipt function
    
    * 🧪 Init test for getTransactionReceipt
    
    * 🏷 Create necessary types & clean types file
    
    * ✨ Init function
    
    * Change type names and add byzantium check
    
    * 🧪 Fix and pass test
    
    * 🎨 DRY'd cleanTransaction(Receipt) functions
    
    * 📝Added documentation to TransactionReceipt type
    
    * ⬆️ Pre-commit script package bump
    
    * 📝Added documentation to Log type
    
    * ✨ Parse logs types and final touches on getTransactionReceipt function
    
    * 🧪 Add web3.js tests to `getTransaction` and `getTransactionReceipt` (#109)
    
    * 🧪  Added web3 test to getTransaction
    
    * ⬆ Pre-commit script package updates
    
    * 🧪 Added web3 test to getTransactionReceipt

--- src/types/Transaction.types.ts
+++ src/types/Transaction.types.ts
@@ -3,11 +3,10 @@ import { TinyBig } from '../shared/tiny-big/tiny-big';
 type Modify<T, R> = Omit<T, keyof R> & R;
 export interface RPCTransaction extends RPCBlockTransaction {
   // not in getBlock transactions, only in getTransaction response
-
   maxFeePerGas: string /* "0xfc21e1832", */;
   maxPriorityFeePerGas: string /* "0x59682f00" */;
 }
-// exact type returned from JSON RPC
+
 export type TransactionResponse = Modify<
   RPCTransaction,
   {
@@ -27,6 +26,41 @@ export type TransactionResponse = Modify<
     confirmations: number;
   }
 >;
+
+/**
+ * Type that contains information from the receipt of a transaction
+ * * Similar to [`Type TransactionReceipt on ethers.providers`](https://docs.ethers.io/v5/api/providers/types/#providers-TransactionReceipt)
+ */
+export type TransactionReceipt = Modify<
+  RPCTransactionReceipt,
+  {
+    blockNumber: number;
+    cumulativeGasUsed: TinyBig;
+    effectiveGasPrice: TinyBig;
+    gasUsed: TinyBig;
+    logs: Array<Log>;
+    status: number;
+    transactionIndex: number;
+    type: number;
+  } & {
+    byzantium: boolean;
+    confirmations: number;
+  }
+>;
+
+/**
+ * Type for the logs that are included in transaction receipts
+ * * Similar to [`Type Log on ethers.providers`](https://docs.ethers.io/v5/api/providers/types/#providers-Log)
+ */
+export type Log = Modify<
+  Omit<RPCLog, 'removed'>,
+  {
+    blockNumber: number;
+    logIndex: number;
+    transactionIndex: number;
+  }
+>;
+
 export type BlockTransactionResponse = Omit<
   TransactionResponse,
   'maxFeePerGas' | 'maxPriorityFeePerGas'
@@ -50,4 +84,33 @@ export interface RPCBlockTransaction {
   type: string;
   v: string;
   value: string;
+}
+
+export interface RPCTransactionReceipt {
+  blockHash: string;
+  blockNumber: string;
+  contractAddress: string;
+  cumulativeGasUsed: string;
+  effectiveGasPrice: string;
+  from: string;
+  gasUsed: string;
+  logs: Array<RPCLog>;
+  logsBloom: string;
+  status: string;
+  to: string;
+  transactionHash: string;
+  transactionIndex: string;
+  type: string;
+}
+
+export interface RPCLog {
+  address: string;
+  blockHash: string;
+  blockNumber: string;
+  data: string;
+  logIndex: string;
+  removed?: boolean;
+  topics: Array<string>;
+  transactionHash: string;
+  transactionIndex: string;
 }
