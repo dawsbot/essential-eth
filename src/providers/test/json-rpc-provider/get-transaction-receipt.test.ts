47b7be3bcea27cba4a423a2d364271770d172ddf
Ari Gibson <hi@arimgibson.com>
2022-04-29 14:46:09 -0600

    🧪 Add web3.js tests to `getTransaction` and `getTransactionReceipt` (#109)
    * 🧪  Added web3 test to getTransaction
    
    * ⬆ Pre-commit script package updates
    
    * 🧪 Added web3 test to getTransactionReceipt

--- src/providers/test/json-rpc-provider/get-transaction-receipt.test.ts
+++ src/providers/test/json-rpc-provider/get-transaction-receipt.test.ts
@@ -1,5 +1,7 @@
 import { ethers } from 'ethers';
 import omit from 'just-omit';
+import Web3 from 'web3';
+import web3core from 'web3-core';
 import { JsonRpcProvider } from '../../../index';
 import { TransactionReceipt } from '../../../types/Transaction.types';
 import { rpcUrls } from '../rpc-urls';
@@ -8,37 +10,102 @@ const rpcUrl = rpcUrls.mainnet;
 
 describe('provider.getTransactionReceipt', () => {
   function testTransactionReceiptEquality(
-    transactionReceipt1: ethers.providers.TransactionReceipt,
+    transactionReceipt1:
+      | ethers.providers.TransactionReceipt
+      | web3core.Transaction,
     transactionReceipt2: TransactionReceipt,
   ) {
-    // requires manually comparing values via bigNum conversion
-    const bignumCheckKeys = [
-      'gasUsed',
-      'cumulativeGasUsed',
-      'effectiveGasPrice',
-    ];
-    const omittedTransactionReceipt1 = omit(transactionReceipt1, [
-      ...bignumCheckKeys,
-    ]);
-    const omittedTransactionReceipt2 = omit(transactionReceipt2, [
-      ...bignumCheckKeys,
-    ]);
+    let typeCheckKeys: Array<string> = [];
+    let omittable1: Array<string> = [];
+    let omittable2: Array<string> = [];
+    if (
+      (transactionReceipt1 as ethers.providers.TransactionReceipt).confirmations
+    ) {
+      // only ethers response has confirmations
+      // requires manually comparing values via bigNum conversion
+      typeCheckKeys = ['gasUsed', 'cumulativeGasUsed', 'effectiveGasPrice'];
+      omittable1 = typeCheckKeys;
+      omittable2 = typeCheckKeys;
+
+      typeCheckKeys.forEach((key) => {
+        expect((transactionReceipt1 as any)[key].toString()).toBe(
+          (transactionReceipt2 as any)[key].toString(),
+        );
+      });
+
+      expect(
+        Math.abs(
+          (transactionReceipt1 as ethers.providers.TransactionReceipt)
+            .confirmations - transactionReceipt2.confirmations,
+        ),
+      ).toBeLessThan(3);
+    } else {
+      typeCheckKeys = [
+        'cumulativeGasUsed',
+        'effectiveGasPrice',
+        'from',
+        'gasUsed',
+        'status',
+        'to',
+        'type',
+      ];
+      omittable1 = typeCheckKeys;
+      omittable2 = ['byzantium', 'confirmations', ...typeCheckKeys];
+
+      typeCheckKeys.forEach((key) => {
+        switch (key) {
+          case 'cumulativeGasUsed':
+          case 'effectiveGasPrice':
+          case 'gasUsed':
+            expect((transactionReceipt1 as any)[key].toString()).toBe(
+              (transactionReceipt2 as any)[key].toString(),
+            );
+            break;
+          case 'from':
+          case 'to':
+            expect((transactionReceipt1 as any)[key]).toBe(
+              (transactionReceipt2 as any)[key].toLowerCase(),
+            );
+            break;
+        }
+      });
+    }
+
+    const omittedTransactionReceipt1 = omit(transactionReceipt1, omittable1) as
+      | TransactionReceipt
+      | web3core.TransactionReceipt;
+    const omittedTransactionReceipt2 = omit(transactionReceipt2, omittable2) as
+      | TransactionReceipt
+      | web3core.TransactionReceipt;
+    omittedTransactionReceipt1.logs = (() => {
+      const logs: any = [];
+      omittedTransactionReceipt1.logs.forEach((log) => {
+        logs.push(omit(log, ['id', 'removed']));
+      });
+      return logs as any;
+    })();
+
     expect(omittedTransactionReceipt1).toStrictEqual(
       omittedTransactionReceipt2,
     );
-    expect(
-      Math.abs(
-        transactionReceipt1.confirmations - transactionReceipt2.confirmations,
-      ),
-    ).toBeLessThan(3);
-    bignumCheckKeys.forEach((key) => {
-      const ethersKey = key as keyof ethers.providers.TransactionResponse;
-      expect((transactionReceipt1 as any)[ethersKey].toString()).toBe(
-        (transactionReceipt2 as any)[key].toString(),
-      );
-    });
   }
-  it('should match ethers and essential-eth', async () => {
+  it('should match web3.js', async () => {
+    const transactionHash =
+      '0x9014ae6ef92464338355a79e5150e542ff9a83e2323318b21f40d6a3e65b4789';
+    const web3Provider = new Web3(rpcUrl);
+    const essentialEthProvider = new JsonRpcProvider(rpcUrl);
+    const [web3TransactionReceipt, essentialEthTransactionReceipt] =
+      await Promise.all([
+        web3Provider.eth.getTransactionReceipt(transactionHash),
+        essentialEthProvider.getTransactionReceipt(transactionHash),
+      ]);
+
+    testTransactionReceiptEquality(
+      web3TransactionReceipt as any,
+      essentialEthTransactionReceipt,
+    );
+  });
+  it('should match ethers', async () => {
     const transactionHash =
       '0x9014ae6ef92464338355a79e5150e542ff9a83e2323318b21f40d6a3e65b4789';
     const ethersProvider = new ethers.providers.StaticJsonRpcProvider(rpcUrl);
