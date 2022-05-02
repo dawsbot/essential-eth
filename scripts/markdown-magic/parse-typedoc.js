78e982e18a9e1515873d328ee0daf9cfeb290f21
Dawson Botsford <dawsonbotsford@gmail.com>
2022-05-01 21:21:38 -0700

    🤓 Markdown magic (utils autodocumented) (#110)
    * so much magic
    
    * auto-generate all utils w/examples too
    
    * nearly ready
    
    * ready
    
    * nearly ready
    
    * cleanup deps
    
    * setup better
    
    * update readme
    
    * add new networks
    
    * readme function links
    
    * fix tsc ignore
    
    * fix warnings
    
    * remove flaky arbitrum test

---
+++ scripts/markdown-magic/parse-typedoc.js
@@ -0,0 +1,81 @@
+/**
+ * Parse a typedoc output file and generate the markdown code
+ * This markdown generated is injected into the root readme via markdown-magic
+ */
+// eslint-disable-next-line @typescript-eslint/no-var-requires
+const stats = require('./typedoc.out.json');
+
+const functions = stats.groups.find((g) => g.title === 'Functions').children;
+
+let functionsMarkdown = '';
+functions.map((functionNumber) => {
+  const child = stats.children.find((child) => child.id === functionNumber);
+
+  const { name } = child; // zeroPad
+
+  const sig = child.signatures[0];
+  const parameters = sig.parameters;
+  let returnType = sig.type.name;
+
+  const examples = sig.comment?.tags
+    ?.filter((tag) => tag.tag === 'example')
+    .map((tag) => {
+      return tag.text;
+    });
+  if (sig.type.type === 'union') {
+    returnType = sig.type.types
+      .map((type) => {
+        const typeName = type.value === null ? 'null' : type.name;
+        return String(typeName);
+      })
+      .join(' | ');
+  }
+
+  const paramsString = parameters
+    .map((parameter) => {
+      let parameterType = parameter.type.name;
+
+      // array types (like "number[]")
+      if (parameter.type.type === 'typeOperator') {
+        parameterType = `Array<${parameter.type.target.elementType.name}>`;
+      }
+
+      // Type unions (like "number | string")
+      if (parameter.type.type === 'union') {
+        parameterType = parameter.type.types
+          .map((type) => type.name)
+          .join(' | ');
+      }
+      return `${parameter.name}: ${parameterType}`;
+    })
+    .join(', ');
+  const examplesMarkdown = examples
+    ? `
+  <details>
+  <summary>View Example</summary>
+
+  \`\`\`js
+  import { ${name} } from 'essential-eth';
+
+  // or in a require environment
+  const { ${name} } = require('essential-eth');
+  \`\`\`
+
+  ${examples.join('')}
+  </details>\n`
+    : '';
+
+  functionsMarkdown += `#### [\`${name}\`](https://essential-eth.vercel.app/docs/api/modules#${name})
+  \`\`\`typescript
+  ${name}(${paramsString}): ${returnType}
+  \`\`\`
+  ${examplesMarkdown}
+  <br/>
+
+`;
+});
+
+module.exports = {
+  functionsMarkdown,
+};
+// fs.writeFileSync('/tmp/out.md', toReturn);
