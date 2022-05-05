498258f838a7ead9921e9bd3b6822d7803c8990f
Ari Gibson <hi@arimgibson.com>
2022-05-05 01:50:33 -0600

    ✨ Added computePublicKey util fn

--- src/utils/compute-public-key.ts
+++ src/utils/compute-public-key.ts
@@ -1,3 +1,7 @@
-export function computePublicKey(privKey: string): string {
-  
+import { Point } from '@noble/secp256k1';
+import { BytesLike, hexlify } from './bytes';
+
+export function computePublicKey(key: BytesLike): string {
+  const bytes = hexlify(key).slice(2);
+  return '0x' + Point.fromPrivateKey(bytes).toHex();
 }
