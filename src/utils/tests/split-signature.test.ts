7e06b6da264cdf70b8b65350fb878db77ad336ca
Ari Gibson <hi@arimgibson.com>
2022-05-13 08:37:01 -0600

    🧪 Add 64 byte test for codecov

--- src/utils/tests/split-signature.test.ts
+++ src/utils/tests/split-signature.test.ts
@@ -3,10 +3,15 @@ import { splitSignature } from '../split-signature';
 
 describe('splitSignature', () => {
   it('should match ethers.js', () => {
-    const signature =
-      '0x60bc4ed91f2021aefe7045f3f77bd12f87eb733aee24bd1965343b3c27b3971647252185b7d2abb411b01b5d1ac4ab41ea486df1e9b396758c1aec6c1b6eee331b';
-    expect(splitSignature(signature)).toStrictEqual(
-      utils.splitSignature(signature),
-    );
+    const signatures = [
+      '0x60bc4ed91f2021aefe7045f3f77bd12f87eb733aee24bd1965343b3c27b3971647252185b7d2abb411b01b5d1ac4ab41ea486df1e9b396758c1aec6c1b6eee331b', //65 byte signature
+      '0x60bc4ed91f2021aefe7045f3f77bd12f87eb733aee24bd1965343b3c27b3971647252185b7d2abb411b01b5d1ac4ab41ea486df1e9b396758c1aec6c1b6eee33', //64 byte signature
+    ];
+
+    signatures.forEach((signature) => {
+      expect(splitSignature(signature)).toStrictEqual(
+        utils.splitSignature(signature),
+      );
+    });
   });
 });
