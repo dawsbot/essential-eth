e2a3f3de93ecd4d561ad912f0045e626b719978f
Ari Gibson <hi@arimgibson.com>
2022-05-26 15:40:55 -0600

    🧪 hexDataLength()

---
+++ src/utils/tests/bytes/hex-data-length.test.ts
@@ -0,0 +1,21 @@
+import { utils as ethers } from 'ethers';
+import { hexDataLength } from '../../bytes';
+
+describe('utils.hexDataLength', () => {
+  it('should match ethers.js - hex values', () => {
+    const values = ['0x9347', '0x185754', '0x00005823'];
+    values.forEach((value) => {
+      expect(hexDataLength(value)).toBe(ethers.hexDataLength(value));
+    });
+  });
+  it('should match ethers.js - UInt8Array values', () => {
+    const values = [
+      [9, 58, 29, 24],
+      [185, 203],
+      [239, 30, 49, 41, 5, 10, 42],
+    ];
+    values.forEach((value) => {
+      expect(hexDataLength(value)).toBe(ethers.hexDataLength(value));
+    });
+  });
+});
