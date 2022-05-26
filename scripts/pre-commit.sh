01e9565656cdac3de6230db533ca906157270247
daws.eth <dawsonbotsford@gmail.com>
2022-05-26 15:55:19 -0700

    🕷 Revert TS version bump

--- scripts/pre-commit.sh
+++ scripts/pre-commit.sh
@@ -1,5 +1,5 @@
 npx npm-check-updates -u  "/.*eslint.*$/"
 npx npm-check-updates -u  "/.*big.*$/"
 npm i
-npm i ethers@latest typescript@latest web3@latest prettier@latest
+npm i ethers@latest web3@latest prettier@latest
 
