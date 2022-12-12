52ac94173973e8f6e264280861196ed17825a285
Turja Chowdhury <42366823+turja-c@users.noreply.github.com>
2022-12-12 11:36:11 -0500

    ✨ Updating packages and passing all tests

--- src/providers/test/fallthrough-provider/get-gas-price.test.ts
+++ src/providers/test/fallthrough-provider/get-gas-price.test.ts
@@ -35,8 +35,8 @@ describe('provider.getGasPrice', () => {
     );
     await timePromise(() => essentialEthProvider.getBlock(14631000)).then(
       (duration) => {
-        // times out the first two requests in 2000 ms each
-        expect(duration).toBeGreaterThan(2000);
+        // times out the first two requests in 1200 ms each
+        expect(duration).toBeGreaterThan(1200);
         // finished the last valid request within two seconds
         expect(duration).toBeLessThan(4000);
       },
