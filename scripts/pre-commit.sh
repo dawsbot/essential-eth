328675ae725c55c7032d7f2db6111b6731cd8b90
dawsbot <dawsonbotsford@gmail.com>
2022-12-20 08:51:55 -0500

    deps cleanup

--- scripts/pre-commit.sh
+++ scripts/pre-commit.sh
@@ -1,4 +1,8 @@
 npx npm-check-updates -u  "/.*big.*$/"
+npx npm-check-updates -u  "/.*prettier.*$/"
+npx npm-check-updates -u  "/.*eslint.*$/"
+npx npm-check-updates -u  "/.*jest.*$/"
+npx npm-check-updates -u  "/.*@swc.*$/"
 npm i
-npm i ethers@latest web3@latest prettier@latest
+npm i --save-dev ethers@latest web3@latest prettier@latest typescript@latest ts-node@latest @swc/core@latest
 
