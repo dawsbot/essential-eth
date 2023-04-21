3994da459e201c6424e8323dfa3abf863955b54d
dawsbot <3408480+dawsbot@users.noreply.github.com>
2023-04-21 13:47:49 +0900

    🤓 Move getBlockNumber test to mocks

--- src/providers/test/json-rpc-provider/get-block-number.test.ts
+++ src/providers/test/json-rpc-provider/get-block-number.test.ts
@@ -1,30 +1,22 @@
-import { StaticJsonRpcProvider } from '@ethersproject/providers';
-import Web3 from 'web3';
+import unfetch from 'isomorphic-unfetch';
 import { JsonRpcProvider } from '../../../index';
+import { mockOf } from '../mock-of';
 
-// RSK has 30 second block times so tests pass more often
-const rpcUrl = `https://public-node.rsk.co`;
-
+jest.mock('isomorphic-unfetch');
+const mockPostResponse = JSON.stringify({
+  jsonrpc: '2.0',
+  id: 1,
+  result: '0xa',
+});
 describe('provider.getBlockNumber', () => {
-  it('should match ethers.js', async () => {
-    const essentialEthProvider = new JsonRpcProvider(rpcUrl);
-    const ethersProvider = new StaticJsonRpcProvider(rpcUrl);
-    const [essentialEthBlockNumber, ethersBlockNumber] = await Promise.all([
-      essentialEthProvider.getBlockNumber(),
-      ethersProvider.getBlockNumber(),
-    ]);
-    // allow one-block flexibility because of timing of requests
-    expect(Math.abs(essentialEthBlockNumber - ethersBlockNumber)).toBeLessThan(
-      2,
+  it('should get number integer', async () => {
+    const essentialEthProvider = new JsonRpcProvider();
+    mockOf(unfetch).mockReturnValueOnce(
+      Promise.resolve({
+        text: () => mockPostResponse,
+      } as unknown as Response),
     );
-  });
-  it('should match web3.js', async () => {
-    const essentialEthProvider = new JsonRpcProvider(rpcUrl);
-    const web3Provider = new Web3(rpcUrl);
-    const [essentialEthBlockNumber, web3BlockNumber] = await Promise.all([
-      essentialEthProvider.getBlockNumber(),
-      web3Provider.eth.getBlockNumber(),
-    ]);
-    expect(essentialEthBlockNumber).toStrictEqual(web3BlockNumber);
+    const essentialEthBlockNumber = await essentialEthProvider.getBlockNumber();
+    expect(essentialEthBlockNumber).toBe(10);
   });
 });
