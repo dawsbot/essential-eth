77eb6a41ab25c183c08f60f8bb8e3c356668bb32
Dawson Botsford <3408480+dawsbot@users.noreply.github.com>
2023-04-20 22:30:33 -0600

    🤓 Add first jest test w/network mock (#211)
    * 🤓 Add first network mock
    
    * Improve test w/zod validation

--- src/providers/test/json-rpc-provider/get-gas-price.test.ts
+++ src/providers/test/json-rpc-provider/get-gas-price.test.ts
@@ -1,19 +1,33 @@
-import { ethers } from 'ethers';
+import unfetch from 'isomorphic-unfetch';
+import z from 'zod';
 import { JsonRpcProvider } from '../../../index';
+import { mockOf } from '../mock-of';
 import { rpcUrls } from '../rpc-urls';
+import { TinyBig } from './../../../shared/tiny-big/tiny-big';
+
+jest.mock('isomorphic-unfetch');
+const postResponse = JSON.stringify({
+  jsonrpc: '2.0',
+  id: 1,
+  result: '0xa',
+});
 
 const rpcUrl = rpcUrls.mainnet;
 
 describe('provider.getGasPrice', () => {
-  it('should match ethers and essential-eth', async () => {
-    const ethersProvider = new ethers.providers.StaticJsonRpcProvider(rpcUrl);
-    const essentialEthProvider = new JsonRpcProvider(rpcUrl);
-    const [ethersGasPrice, essentialEthGasPrice] = await Promise.all([
-      ethersProvider.getGasPrice(),
-      essentialEthProvider.getGasPrice(),
-    ]);
+  it('should get integer of gas price', async () => {
+    const provider = new JsonRpcProvider(rpcUrl);
+    mockOf(unfetch).mockReturnValueOnce(
+      Promise.resolve({
+        text: () => postResponse,
+      } as unknown as Response),
+    );
+
+    const gasPrice = await provider.getGasPrice();
+    expect(z.instanceof(TinyBig).safeParse(gasPrice).success).toBe(true);
     expect(
-      ethersGasPrice.sub(essentialEthGasPrice.toString()).toNumber(),
-    ).toBeLessThan(100);
+      z.number().int().positive().safeParse(gasPrice.toNumber()).success,
+    ).toBe(true);
+    expect(gasPrice.toString()).toBe('10');
   });
 });
