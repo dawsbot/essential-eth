1b587515338553c7218541e587f0388140b8150d
Jacob T Firek <106350168+jtfirek@users.noreply.github.com>
2023-04-25 09:25:52 -0400

    🧪 Mocking Tests for getFeeData (#215)
    * adding new mock test back
    
    * add back readme
    
    * Remove tsignores
    
    * add back readme
    
    ---------
    
    Co-authored-by: dawsbot <3408480+dawsbot@users.noreply.github.com>

--- src/providers/test/json-rpc-provider/get-fee-data.test.ts
+++ src/providers/test/json-rpc-provider/get-fee-data.test.ts
@@ -1,35 +1,65 @@
-import { StaticJsonRpcProvider } from '@ethersproject/providers';
+import * as unfetch from 'isomorphic-unfetch';
+import {
+  buildFetchInit,
+  buildRPCPostBody,
+} from '../../../classes/utils/fetchers';
+import type { TinyBig } from '../../../shared/tiny-big/tiny-big';
 import { JsonRpcProvider } from '../../JsonRpcProvider';
+import { mockOf } from '../mock-of';
 import { rpcUrls } from './../rpc-urls';
 
 const rpcUrl = rpcUrls.mainnet;
 
 const essentialEthProvider = new JsonRpcProvider(rpcUrl);
-const ethersProvider = new StaticJsonRpcProvider(rpcUrl);
+
+jest.mock('isomorphic-unfetch');
+//  essentialEthProvider.getFeeData() calls these methods internally
+const mockGetBlockResponse = JSON.stringify({
+  jsonrpc: '2.0',
+  id: 1,
+  result: {
+    baseFeePerGas: '0xa',
+    // add more block data here if needed
+  },
+});
+const mockGetGasPriceResponse = JSON.stringify({
+  jsonrpc: '2.0',
+  id: 1,
+  result: '0xa',
+});
+
 describe('provider.getFeeData', () => {
-  it('should match ethers.js', async () => {
-    const [ethersFeeData, eeFeeData] = await Promise.all([
-      ethersProvider.getFeeData(),
-      essentialEthProvider.getFeeData(),
-    ]);
-    expect(eeFeeData.gasPrice.toString()).toBe(
-      // @ts-ignore
-      ethersFeeData?.gasPrice.toString(),
+  it('should match mocked responses', async () => {
+    mockOf(unfetch.default).mockResolvedValueOnce({
+      text: () => Promise.resolve(mockGetBlockResponse),
+    } as Response);
+    mockOf(unfetch.default).mockResolvedValueOnce({
+      text: () => Promise.resolve(mockGetGasPriceResponse),
+    } as Response);
+    const spy = jest.spyOn(unfetch, 'default');
+
+    const feeData = (await essentialEthProvider.getFeeData()) as {
+      gasPrice: TinyBig;
+      lastBaseFeePerGas: TinyBig;
+      maxFeePerGas: TinyBig;
+      maxPriorityFeePerGas: TinyBig;
+    };
+    expect(feeData.gasPrice.toString()).toBe('10');
+    // lastBaseFeePerGas should be equal to the mocked baseFeePerGas value
+    expect(feeData.lastBaseFeePerGas.toString()).toBe('10');
+    // maxFeePerGas is calculated as (baseFeePerGas * 2) + maxPriorityFeePerGas, (10 * 2) + 1500000000 = 1500000020
+    expect(feeData.maxFeePerGas.toString()).toBe('1500000020');
+    // maxPriorityFeePerGas is a constant value (1500000000) in the getFeeData function
+    expect(feeData.maxPriorityFeePerGas.toString()).toBe('1500000000');
+    expect(spy).toHaveBeenCalledWith(
+      rpcUrl,
+      buildFetchInit(
+        buildRPCPostBody('eth_getBlockByNumber', ['latest', false]),
+      ),
     );
-    // @ts-ignore
-    expect(eeFeeData.lastBaseFeePerGas.toString()).toBe(
-      // @ts-ignore
-      ethersFeeData?.lastBaseFeePerGas.toString(),
-    );
-    // @ts-ignore
-    expect(eeFeeData.maxFeePerGas.toString()).toBe(
-      // @ts-ignore
-      ethersFeeData?.maxFeePerGas.toString(),
-    );
-    // @ts-ignore
-    expect(eeFeeData.maxPriorityFeePerGas.toString()).toBe(
-      // @ts-ignore
-      ethersFeeData?.maxPriorityFeePerGas.toString(),
+    expect(spy).toHaveBeenCalledWith(
+      rpcUrl,
+      buildFetchInit(buildRPCPostBody('eth_gasPrice', [])),
     );
   });
 });
