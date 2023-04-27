5232e3b5c68df95a7fb2e7f526dc07d26db3994c
Jacob Firek <jacob@interlay.io>
2023-04-27 14:26:12 -0400

    get-transaction mock

--- src/providers/test/json-rpc-provider/get-transaction.test.ts
+++ src/providers/test/json-rpc-provider/get-transaction.test.ts
@@ -1,68 +1,61 @@
-import { ethers } from 'ethers';
 import omit from 'just-omit';
-import Web3 from 'web3';
-import type web3core from 'web3-core';
 import { hexToDecimal } from '../../../classes/utils/hex-to-decimal';
 import { JsonRpcProvider, tinyBig } from '../../../index';
 import type { TransactionResponse } from '../../../types/Transaction.types';
 import { rpcUrls } from '../rpc-urls';
+import * as unfetch from 'isomorphic-unfetch';
+import { mockOf } from '../mock-of';
+import { cleanTransaction } from '../../../classes/utils/clean-transaction';
+import { buildFetchInit, buildRPCPostBody } from '../../../classes/utils/fetchers';
 
 const rpcUrl = rpcUrls.mainnet;
 
+jest.mock('isomorphic-unfetch');
+
+const transactionHash =
+  '0x9014ae6ef92464338355a79e5150e542ff9a83e2323318b21f40d6a3e65b4789';
+
+const mockTransaction = {
+  blockHash: '0x1234567890abcdef1234567890abcdef1234567890abcdef1234567890abcdef',
+  blockNumber: '0x1',
+  from: '0x742d35Cc6634C0532925a3b844Bc454e4438f44e',
+  gas: '0x5208',
+  gasPrice: '0x2540be400',
+  maxFeePerGas: '0x34e15f86', 
+  maxPriorityFeePerGas: '0x5d21dba0',
+  hash: transactionHash,
+  input: '0x',
+  nonce: '0x1',
+  to: '0x3535353535353535353535353535353535353535',
+  transactionIndex: '0x0',
+  value: '0x1',
+  v: '0x25',
+  r: '0x3d71c9ac9e9382a652a96217c3ee7a4b4f75ec86d4c6a4ad5754a13c1eb14d19',
+  s: '0x7e0f08a62d7c1150238e2bc1b9d6d49fa6c1758c0f60e0d6646d30e7b6598a2e',
+  chainId: '1',  
+  type: '0x1'
+}
+
+const mockRpcTransactionResponse = JSON.stringify({
+  jsonrpc: '2.0',
+  id: 1,
+  result: mockTransaction
+});
+
+const mockBlockResponse = JSON.stringify({
+  jsonrpc: '2.0',
+  id: 1,
+  result: {
+    number: '0x3',
+    hash: '0xabcdef1234567890abcdef1234567890abcdef1234567890abcdef1234567890',
+  }
+});
+
 describe('provider.getTransaction', () => {
   function testTransactionEquality(
-    transaction1: ethers.providers.TransactionResponse | web3core.Transaction,
-    transaction2: TransactionResponse,
+    testTransaction: TransactionResponse,
   ) {
-    let numCheckKeys: Array<string> = [];
-    let omittable1: Array<string> = [];
-    let omittable2: Array<string> = [];
-    if ((transaction1 as ethers.providers.TransactionResponse).confirmations) {
-      // only the ethers response has confirmations
-      // requires manually comparing values via bigNum conversion
-      numCheckKeys = [
-        'nonce',
-        'value',
-        'gas',
-        'gasPrice',
-        'maxFeePerGas',
-        'maxPriorityFeePerGas',
-        'confirmations',
-      ];
-
-      omittable1 = [
-        'wait', // ethers injects this to allow you to wait on a certain confirmation count
-        'creates', // ethers injects this custom https://github.com/ethers-io/ethers.js/blob/948f77050dae884fe88932fd88af75560aac9d78/packages/providers/src.ts/formatter.ts#L336
-        'data', // ethers renames input to data https://github.com/ethers-io/ethers.js/blob/948f77050dae884fe88932fd88af75560aac9d78/packages/providers/src.ts/formatter.ts#L331
-        'gasLimit', // ethers renames gas to gasLimit https://github.com/ethers-io/ethers.js/blob/948f77050dae884fe88932fd88af75560aac9d78/packages/providers/src.ts/formatter.ts#L320
-        'input',
-        ...numCheckKeys,
-      ];
-
-      omittable2 = ['input', ...numCheckKeys];
-
-      numCheckKeys.forEach((key) => {
-        let ethersKey = key as keyof ethers.providers.TransactionResponse;
-        if (key === 'gas') {
-          ethersKey = 'gasLimit';
-        }
-        // give small room for error in tests
-        expect(
-          tinyBig((transaction1 as any)[ethersKey])
-            .minus(tinyBig((transaction2 as any)[key]))
-            .abs()
-            .lt(2),
-        ).toBe(true);
-      });
-
-      expect(
-        Math.abs(
-          (transaction1 as ethers.providers.TransactionResponse).confirmations -
-            transaction2.confirmations,
-        ),
-      ).toBeLessThan(3);
-    } else {
-      numCheckKeys = [
+      const numCheckKeys = [
         'chainId',
         'gas',
         'gasPrice',
@@ -71,55 +64,59 @@ describe('provider.getTransaction', () => {
         'nonce',
         'v',
         'value',
+        'confirmations',
       ];
-      omittable1 = [...numCheckKeys];
-      omittable2 = ['confirmations', ...numCheckKeys];
+      const omittable1 = [...numCheckKeys];
+      const omittable2 = ['confirmations', ...numCheckKeys];
+      const expectedTransactionResponse = cleanTransaction(mockTransaction);
+      // confirmations = (currentBlock - transactionBlockNumber) + 1
+      // confirmations = (3 - 1) + 1 = 3
+      expectedTransactionResponse.confirmations = 3;
 
       numCheckKeys.forEach((key) => {
         if (
-          typeof (transaction1 as any)[key] === 'string' &&
-          (transaction1 as any)[key].startsWith('0x')
+          typeof (expectedTransactionResponse as any)[key] === 'string' &&
+          (expectedTransactionResponse as any)[key].startsWith('0x')
         ) {
-          (transaction1 as any)[key] = Number(
-            hexToDecimal((transaction1 as any)[key]),
+          (expectedTransactionResponse as any)[key] = Number(
+            hexToDecimal((expectedTransactionResponse as any)[key]),
           );
         }
         // give room for error in tests
         expect(
-          tinyBig((transaction1 as any)[key])
-            .minus(tinyBig((transaction2 as any)[key]))
+          tinyBig((expectedTransactionResponse as any)[key])
+            .minus(tinyBig((testTransaction as any)[key]))
             .abs()
             .lt(2),
         ).toBe(true);
       });
-    }
 
-    const omittedTransaction1 = omit(transaction1, omittable1 as any);
-    const omittedTransaction2 = omit(transaction2, omittable2 as any);
-    expect(omittedTransaction1).toMatchObject(omittedTransaction2);
+    const omittedexpectedTransactionResponse = omit(expectedTransactionResponse, omittable1 as any);
+    const omittedTransaction2 = omit(testTransaction, omittable2 as any);
+    expect(omittedexpectedTransactionResponse).toMatchObject(omittedTransaction2);
   }
-  it('should match web3.js', async () => {
-    const transactionHash =
-      '0x9014ae6ef92464338355a79e5150e542ff9a83e2323318b21f40d6a3e65b4789';
-    const web3Provider = new Web3(rpcUrl);
-    const essentialEthProvider = new JsonRpcProvider(rpcUrl);
-    const [web3Transaction, essentialEthTransaction] = await Promise.all([
-      web3Provider.eth.getTransaction(transactionHash),
-      essentialEthProvider.getTransaction(transactionHash),
-    ]);
 
-    testTransactionEquality(web3Transaction, essentialEthTransaction);
-  });
-  it('should match ethers.js', async () => {
-    const transactionHash =
-      '0x9014ae6ef92464338355a79e5150e542ff9a83e2323318b21f40d6a3e65b4789';
-    const ethersProvider = new ethers.providers.StaticJsonRpcProvider(rpcUrl);
+  it('should match our expected transaction', async () => {
     const essentialEthProvider = new JsonRpcProvider(rpcUrl);
-    const [ethersTransaction, essentialEthTransaction] = await Promise.all([
-      ethersProvider.getTransaction(transactionHash),
-      essentialEthProvider.getTransaction(transactionHash),
-    ]);
-
-    testTransactionEquality(ethersTransaction, essentialEthTransaction);
+    mockOf(unfetch.default).mockResolvedValueOnce({
+      text: () => Promise.resolve(mockRpcTransactionResponse),
+    } as Response);
+    mockOf(unfetch.default).mockResolvedValueOnce({
+      text: () => Promise.resolve(mockBlockResponse),
+    } as Response);
+    const spy = jest.spyOn(unfetch, 'default');
+    
+    const essentialEthTransaction = await essentialEthProvider.getTransaction( transactionHash );
+  
+    testTransactionEquality(essentialEthTransaction);
+    
+    expect(spy).toHaveBeenCalledWith(
+      rpcUrl,
+      buildFetchInit(buildRPCPostBody('eth_getTransactionByHash', [transactionHash])),
+    );
+    expect(spy).toHaveBeenCalledWith(
+      rpcUrl,
+      buildFetchInit(buildRPCPostBody('eth_getBlockByNumber', ['latest', false])),
+    );
   });
 });
