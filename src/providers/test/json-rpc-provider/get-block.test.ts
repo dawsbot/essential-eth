d066f8e5c15b61ff8e55047afb8063ba8fd44985
Jacob T Firek <106350168+jtfirek@users.noreply.github.com>
2023-05-12 01:00:04 -0400

    🧪 mocking get-code (#230)
    * working on mocking get code
    
    * broken for now
    
    * not working drat
    
    * refactored code
    
    * prettier
    
    ---------
    
    Co-authored-by: dawsbot <dawsonbotsford@gmail.com>

--- src/providers/test/json-rpc-provider/get-block.test.ts
+++ src/providers/test/json-rpc-provider/get-block.test.ts
@@ -1,12 +1,13 @@
 import * as unfetch from 'isomorphic-unfetch';
-import {
-  RPCMethodName,
-  buildFetchInit,
-  buildRPCPostBody,
-} from '../../../classes/utils/fetchers';
 import { JsonRpcProvider, tinyBig, toChecksumAddress } from '../../..';
-import { mockOf } from '../mock-of';
+import type {
+  RPCMethodName} from '../../../classes/utils/fetchers';
+import {
+  buildFetchInit,
+  buildRPCPostBody
+} from '../../../classes/utils/fetchers';
 import { hexToDecimal } from '../../../classes/utils/hex-to-decimal';
+import { mockOf } from '../mock-of';
 import { rpcUrls } from '../rpc-urls';
 
 jest.mock('isomorphic-unfetch');
@@ -18,22 +19,24 @@ const provider = new JsonRpcProvider(rpcUrl);
 const mockBlockResponse = {
   number: '0x1b4',
   hash: '0x5f6fb043528e9892679bce31e9e4a9c5773b3b1ebad1dc4c533ed6fe75ebe13d',
-  parentHash: '0x4e61f7d8fc6253a08d0762dbb2d3d5f4a7e0a2394a2d29c4bbe7056aa13d48e8',
+  parentHash:
+    '0x4e61f7d8fc6253a08d0762dbb2d3d5f4a7e0a2394a2d29c4bbe7056aa13d48e8',
   nonce: '0xe04d296d2460cfb8472af2c5fd05b5a214109c25688d3704aed5484f9a7792f2',
-  sha3Uncles: '0x1dcc4de8dec75d7aab85b567b6ccd41ad312451b948a7413f0a142fd40d49347',
+  sha3Uncles:
+    '0x1dcc4de8dec75d7aab85b567b6ccd41ad312451b948a7413f0a142fd40d49347',
   logsBloom: '0x056e68544253...',
   transactionsRoot: '0x9156485c5d...',
   stateRoot: '0x2f8bde4d1b...',
   miner: '0x4e65fda2159562a496f9f3522f89122a3088497a',
   difficulty: '0x027f07',
-  totalDifficulty:  '0x027f07',
+  totalDifficulty: '0x027f07',
   extraData: '0x000000000000...',
-  size:  '0x027f07',
+  size: '0x027f07',
   gasLimit: '0x9f759',
   gasUsed: '0x9f759',
   timestamp: '0x54e34e8e',
   transactions: [],
-  uncles: []
+  uncles: [],
 };
 const mockRpcBlockResponse = JSON.stringify({
   jsonrpc: '2.0',
@@ -52,7 +55,11 @@ const mockBlock = {
   timestamp: tinyBig(mockBlockResponse.timestamp),
 };
 
-async function runTest(method: RPCMethodName, params: (string | number | boolean)[], responseIdentifier: string | number): Promise<void> {
+async function runTest(
+  method: RPCMethodName,
+  params: (string | number | boolean)[],
+  responseIdentifier: string | number,
+): Promise<void> {
   jest.clearAllMocks();
   mockOf(unfetch.default).mockResolvedValueOnce({
     text: () => Promise.resolve(mockRpcBlockResponse),
@@ -79,7 +86,11 @@ describe('provider.getBlock', () => {
 
   const blockNumber = 1000000; // a certain block number for testing
   it(`should match mocked block -- specific block number as decimal integer. (block #${blockNumber})`, async () => {
-    await runTest('eth_getBlockByNumber', [tinyBig(blockNumber).toHexString(), false], blockNumber);
+    await runTest(
+      'eth_getBlockByNumber',
+      [tinyBig(blockNumber).toHexString(), false],
+      blockNumber,
+    );
   });
 
   const blockHash =
@@ -92,15 +103,13 @@ describe('provider.getBlock', () => {
 describe('provider.getBlock error handling', () => {
   it('should handle empty 200 http response', async () => {
     mockOf(unfetch.default).mockResolvedValueOnce({
-      text: () => Promise.resolve("200 OK"),
+      text: () => Promise.resolve('200 OK'),
     } as Response);
 
     const spy = jest.spyOn(unfetch, 'default');
     await provider.getBlock('earliest').catch(async (error) => {
       // error message is Invalid JSON RPC response: "200 OK"
-      expect('Invalid JSON RPC response: "200 OK"').toBe(
-        error.message,
-      );
+      expect('Invalid JSON RPC response: "200 OK"').toBe(error.message);
     });
 
     expect(spy).toHaveBeenCalledWith(
@@ -113,20 +122,20 @@ describe('provider.getBlock error handling', () => {
 
   it('should handle empty JSON object', async () => {
     mockOf(unfetch.default).mockResolvedValueOnce({
-      text: () => Promise.resolve("{}"),
+      text: () => Promise.resolve('{}'),
     } as Response);
-  
+
     const spy = jest.spyOn(unfetch, 'default');
     await provider.getBlock('earliest').catch(async (error) => {
       // error message is Invalid JSON RPC response: {}
       expect(error.message).toBe('Invalid JSON RPC response: {}');
     });
-  
+
     expect(spy).toHaveBeenCalledWith(
       rpcUrl,
       buildFetchInit(
         buildRPCPostBody('eth_getBlockByNumber', ['earliest', false]),
       ),
     );
-  });  
+  });
 });
