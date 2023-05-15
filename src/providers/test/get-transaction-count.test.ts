fbeeb328039e5ce27e17f9010239b76ac1ccd7e0
Jacob Firek <jacob@interlay.io>
2023-05-15 15:10:10 -0500

    mocking get-transaction-count

--- src/providers/test/get-transaction-count.test.ts
+++ src/providers/test/get-transaction-count.test.ts
@@ -1,47 +1,56 @@
-import { ethers } from 'ethers';
-import Web3 from 'web3';
-import { jsonRpcProvider } from '../..';
+import * as unfetch from 'isomorphic-unfetch';
+import { jsonRpcProvider, tinyBig } from '../..';
 import type { BlockTag } from '../../types/Block.types';
+import { mockOf } from './mock-of';
 import { rpcUrls } from './rpc-urls';
+import { buildFetchInit, buildRPCPostBody } from '../../classes/utils/fetchers';
+
+jest.mock('isomorphic-unfetch');
 
 // coinbase 1 hotwallet
 const address = '0x71660c4005ba85c37ccec55d0c4493e66fe775d3';
-/**
- *
- * @param rpcUrl
- * @param blockTag
- */
+
 async function testGetTC(rpcUrl: string, blockTag?: BlockTag) {
-  const eeProvider = jsonRpcProvider(rpcUrl);
-  const ethersProvider = new ethers.providers.StaticJsonRpcProvider(rpcUrl);
-  const web3Provider = new Web3(rpcUrl);
-  const [eeTC, ethersTC, web3TC] = await Promise.all([
-    eeProvider.getTransactionCount(address, blockTag),
-    ethersProvider.getTransactionCount(address, blockTag),
-    web3Provider.eth.getTransactionCount(address, blockTag as any),
-  ]);
-  if (typeof blockTag === 'number') {
-    // a magic-number, no significance
-    expect(eeTC).toBe(1053312);
-  }
-  expect(eeTC).toBe(ethersTC);
-  expect(eeTC).toBe(web3TC);
+  const provider = jsonRpcProvider(rpcUrl);
+  const mockPostResponse = JSON.stringify({
+    jsonrpc: '2.0',
+    id: 1,
+    result: '0xa',
+  });
+
+  mockOf(unfetch.default).mockResolvedValueOnce({
+    text: () => Promise.resolve(mockPostResponse),
+  } as Response);
+
+  const spy = jest.spyOn(unfetch, 'default');
+
+  const transactionCount = await provider.getTransactionCount(address, blockTag);
+
+  expect(transactionCount.toString()).toBe('10');
+  
+  const expectedBlockTag = typeof blockTag === 'number'
+  ? tinyBig(blockTag).toHexString()
+  : (blockTag ?? 'latest');
+  expect(spy).toHaveBeenCalledWith(
+    rpcUrl,
+    buildFetchInit(
+      buildRPCPostBody('eth_getTransactionCount', [address, expectedBlockTag]),
+    ),
+  );
 }
-describe('provider.getBalance mainnet', () => {
+
+describe('provider.getTransactionCount mainnet', () => {
   const rpcUrl = rpcUrls.matic;
-  it('should get latest equal to ethers and web3', async () => {
+  it('should get the latest by the latest block', async () => {
     await testGetTC(rpcUrl, 'latest');
   });
-  it('should get default latest equal to ethers and web3', async () => {
+  it('should get transaction count by default latest block', async () => {
     await testGetTC(rpcUrl);
   });
-  it('should get earliest equal to ethers and web3', async () => {
+  it('should get transaction count by earliest block', async () => {
     await testGetTC(rpcUrl, 'earliest');
   });
-  // re-enable when we have an archive node
-  // fast-sync nodes are cheap and cannot compute old block data like this
-  // Yields Error "missing trie node"
-  // it('should tx count up to block number equal to ethers and web3', async () => {
-  //   await testGetTC(rpcUrl, 14649390);
-  // });
+  it('should get TC by block number', async () => {
+    await testGetTC(rpcUrl, 14649390);
+  });
 });
