8566f1b4b667ae32a08574fb7078c1d0b5a4e92b
dawsbot <dawsonbotsford@gmail.com>
2023-06-06 15:36:47 -0600

    Fix missing buffer

--- src/utils/solidity-keccak256.ts
+++ src/utils/solidity-keccak256.ts
@@ -1,4 +1,3 @@
-import { Buffer } from 'buffer';
 import { hexFalse } from '../classes/utils/encode-decode-transaction';
 import { logger } from '../logger/logger';
 import { tinyBig } from '../shared/tiny-big/tiny-big';
