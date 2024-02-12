5ff82b82c4dd84ff88ab4fd47dfa2be79bae84a5
dawsbot <dawsonbotsford@gmail.com>
2024-02-12 15:05:28 -0700

    un-module

--- src/index.ts
+++ src/index.ts
@@ -1,20 +1,20 @@
-import { BaseContract, Contract } from './classes/Contract';
-import { AlchemyProvider } from './providers/AlchemyProvider';
-import {
+export { BaseContract, Contract } from './classes/Contract';
+export { AlchemyProvider } from './providers/AlchemyProvider';
+export {
   ConstructorOptions,
   FallthroughProvider,
 } from './providers/FallthroughProvider';
-import { JsonRpcProvider, jsonRpcProvider } from './providers/JsonRpcProvider';
-import { TinyBig, tinyBig } from './shared/tiny-big/tiny-big';
-import { BlockResponse, BlockTag, RPCBlock } from './types/Block.types';
-import {
+export { JsonRpcProvider, jsonRpcProvider } from './providers/JsonRpcProvider';
+export { TinyBig, tinyBig } from './shared/tiny-big/tiny-big';
+export type { BlockResponse, BlockTag, RPCBlock } from './types/Block.types';
+export type {
   ContractTypes,
   JSONABI,
   JSONABIArgument,
 } from './types/Contract.types';
-import { Filter, FilterByBlockHash } from './types/Filter.types';
-import { Network } from './types/Network.types';
-import {
+export type { Filter, FilterByBlockHash } from './types/Filter.types';
+export type { Network } from './types/Network.types';
+export type {
   BlockTransactionResponse,
   Log,
   RPCLog,
@@ -25,61 +25,19 @@ import {
   TransactionRequest,
   TransactionResponse,
 } from './types/Transaction.types';
-import { computeAddress } from './utils/compute-address';
-import { computePublicKey } from './utils/compute-public-key';
-import { etherToGwei } from './utils/ether-to-gwei';
-import { etherToWei } from './utils/ether-to-wei';
-import { gweiToEther } from './utils/gwei-to-ether';
-import { hashMessage } from './utils/hash-message';
-import { isAddress } from './utils/is-address';
-import { splitSignature } from './utils/split-signature';
-import { toChecksumAddress } from './utils/to-checksum-address';
-import { toUtf8Bytes } from './utils/to-utf8-bytes';
-import { weiToEther } from './utils/wei-to-ether';
+export { computeAddress } from './utils/compute-address';
+export { computePublicKey } from './utils/compute-public-key';
+export { etherToGwei } from './utils/ether-to-gwei';
+export { etherToWei } from './utils/ether-to-wei';
+export { gweiToEther } from './utils/gwei-to-ether';
+export { hashMessage } from './utils/hash-message';
+export { isAddress } from './utils/is-address';
+export { splitSignature } from './utils/split-signature';
+export { toChecksumAddress } from './utils/to-checksum-address';
+export { toUtf8Bytes } from './utils/to-utf8-bytes';
+export { weiToEther } from './utils/wei-to-ether';
 
 export * from './utils/bytes';
 export * from './utils/hash-message';
 export * from './utils/keccak256';
-export * from './utils/solidity-keccak256';
-export {
-  etherToWei,
-  etherToGwei,
-  isAddress,
-  jsonRpcProvider,
-  JsonRpcProvider,
-  FallthroughProvider,
-  AlchemyProvider,
-  tinyBig,
-  toChecksumAddress,
-  weiToEther,
-  gweiToEther,
-  hashMessage,
-  splitSignature,
-  toUtf8Bytes,
-  computeAddress,
-  computePublicKey,
-  /* classes */
-  Contract,
-  TinyBig,
-  /* types */
-  BaseContract,
-  BlockResponse,
-  ContractTypes,
-  Filter,
-  FilterByBlockHash,
-  JSONABI,
-  JSONABIArgument,
-  Network,
-  TransactionResponse,
-  RPCBlock,
-  RPCTransaction,
-  RPCTransactionReceipt,
-  TransactionRequest,
-  RPCTransactionRequest,
-  TransactionReceipt,
-  BlockTag,
-  RPCLog,
-  Log,
-  BlockTransactionResponse,
-  ConstructorOptions,
-};
+export { pack, solidityKeccak256 } from './utils/solidity-keccak256';
