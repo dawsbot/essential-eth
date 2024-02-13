1787432d1269f363d3f8677a474bf2c3ec19d466
dawsbot <3408480+dawsbot@users.noreply.github.com>
2024-02-13 15:54:36 -0700

    Change export style

--- src/index.ts
+++ src/index.ts
@@ -1,85 +1,43 @@
-import { BaseContract, Contract } from './classes/Contract';
-import { AlchemyProvider } from './providers/AlchemyProvider';
-import {
-  ConstructorOptions,
+export { Contract, BaseContract } from './classes/Contract';
+export { AlchemyProvider } from './providers/AlchemyProvider';
+export {
   FallthroughProvider,
+  ConstructorOptions,
 } from './providers/FallthroughProvider';
-import { JsonRpcProvider, jsonRpcProvider } from './providers/JsonRpcProvider';
-import { TinyBig, tinyBig } from './shared/tiny-big/tiny-big';
-import { BlockResponse, BlockTag, RPCBlock } from './types/Block.types';
-import {
+export { jsonRpcProvider, JsonRpcProvider } from './providers/JsonRpcProvider';
+export { tinyBig, TinyBig } from './shared/tiny-big/tiny-big';
+export { BlockResponse, RPCBlock, BlockTag } from './types/Block.types';
+export {
   ContractTypes,
   JSONABI,
   JSONABIArgument,
 } from './types/Contract.types';
-import { Filter, FilterByBlockHash } from './types/Filter.types';
-import { Network } from './types/Network.types';
-import {
-  BlockTransactionResponse,
-  Log,
-  RPCLog,
+export { Filter, FilterByBlockHash } from './types/Filter.types';
+export { Network } from './types/Network.types';
+export {
+  TransactionResponse,
   RPCTransaction,
   RPCTransactionReceipt,
+  TransactionRequest,
   RPCTransactionRequest,
   TransactionReceipt,
-  TransactionRequest,
-  TransactionResponse,
+  RPCLog,
+  Log,
+  BlockTransactionResponse,
 } from './types/Transaction.types';
-import { computeAddress } from './utils/compute-address';
-import { computePublicKey } from './utils/compute-public-key';
-import { etherToGwei } from './utils/ether-to-gwei';
-import { etherToWei } from './utils/ether-to-wei';
-import { gweiToEther } from './utils/gwei-to-ether';
-import { hashMessage } from './utils/hash-message';
-import { isAddress } from './utils/is-address';
-import { splitSignature } from './utils/split-signature';
-import { toChecksumAddress } from './utils/to-checksum-address';
-import { toUtf8Bytes } from './utils/to-utf8-bytes';
-import { weiToEther } from './utils/wei-to-ether';
+export { computeAddress } from './utils/compute-address';
+export { computePublicKey } from './utils/compute-public-key';
+export { etherToGwei } from './utils/ether-to-gwei';
+export { etherToWei } from './utils/ether-to-wei';
+export { gweiToEther } from './utils/gwei-to-ether';
+export { hashMessage } from './utils/hash-message';
+export { isAddress } from './utils/is-address';
+export { splitSignature } from './utils/split-signature';
+export { toChecksumAddress } from './utils/to-checksum-address';
+export { toUtf8Bytes } from './utils/to-utf8-bytes';
+export { weiToEther } from './utils/wei-to-ether';
 
 export * from './utils/bytes';
 export * from './utils/hash-message';
 export * from './utils/keccak256';
 export * from './utils/solidity-keccak256';
-export {
-  etherToWei,
-  etherToGwei,
-  isAddress,
-  jsonRpcProvider,
-  JsonRpcProvider,
-  FallthroughProvider,
-  AlchemyProvider,
-  tinyBig,
-  toChecksumAddress,
-  weiToEther,
-  gweiToEther,
-  hashMessage,
-  splitSignature,
-  toUtf8Bytes,
-  computeAddress,
-  computePublicKey,
-  /* classes */
-  Contract,
-  TinyBig,
-  /* types */
-  BaseContract,
-  BlockResponse,
-  ContractTypes,
-  Filter,
-  FilterByBlockHash,
-  JSONABI,
-  JSONABIArgument,
-  Network,
-  TransactionResponse,
-  RPCBlock,
-  RPCTransaction,
-  RPCTransactionReceipt,
-  TransactionRequest,
-  RPCTransactionRequest,
-  TransactionReceipt,
-  BlockTag,
-  RPCLog,
-  Log,
-  BlockTransactionResponse,
-  ConstructorOptions,
-};
