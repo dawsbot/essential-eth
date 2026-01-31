54b05b8665c2603cba1659881f87fec30177f822
dawsbot <3408480+dawsbot@users.noreply.github.com>
2026-01-31 12:39:45 -0700

    fix site building

---
+++ docs-site/.astro/content-assets.mjs
@@ -0,0 +1,4 @@
+
+import __ASTRO_IMAGE_IMPORT_1P4XAj from "../../assets/logo.png?astroContentImageFlag=&importer=src%2Fcontent%2Fdocs%2Findex.mdx";
+export default new Map([["../../assets/logo.png?astroContentImageFlag=&importer=src%2Fcontent%2Fdocs%2Findex.mdx", __ASTRO_IMAGE_IMPORT_1P4XAj]]);
+		
