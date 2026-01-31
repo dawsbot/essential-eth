1e6267687a1a4eb3a203d156e961cc5bed333423
dawsbot <3408480+dawsbot@users.noreply.github.com>
2026-01-31 11:33:38 -0700

    upgrade codemod detection for viem

---
+++ scripts/migrate-test-fixture/types.ts
@@ -0,0 +1,7 @@
+import { type Address, type Hash, type Hex } from 'viem';
+
+const addr: Address = '0x1234567890abcdef1234567890abcdef12345678';
+const tx: Hash = '0xabcdef';
+const data: Hex = '0xff';
+
+console.log(addr, tx, data);
